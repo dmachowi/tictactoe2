721999fc5184280f15829cc4fb4b991557d9f4a0
DREWCIFER <drewcifer@imacdifederico.lan>
2014-07-17 19:48:16 +0200

    Initial Commit

---
+++ tictactoe2/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  tictactoe2
+//
+//  Created by DREWCIFER on 17/07/14.
+//  Copyright (c) 2014 dmm. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
